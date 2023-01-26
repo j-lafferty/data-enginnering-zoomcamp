3a0ff5aa9895de2c136f5c26e314be81e0abcd71
james lafferty <lafferty.james@icloud.com>
2023-01-26 18:25:29 +0000

    add etl to gcs workflow

--- week_2_workflow_orchestration/ingest_data_flow.py
+++ week_2_workflow_orchestration/ingest_data_flow.py
@@ -21,8 +21,8 @@ def extract_data(url: str):
     # the backup files are gzipped, and it's important to keep the correct extension
     # for pandas to be able to open the file
     if url.endswith(".csv.gz"):
-        csv_name = "yellow_tripdata_2021-01.csv.gz"
-    else:
+        csv_name = "output.csv.gz"
+    elif url.endswith(".csv"):
         csv_name = "output.csv"
 
     os.system(f"wget {url} -O {csv_name}")
