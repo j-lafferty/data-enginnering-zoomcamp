079b717b2f9df66643118eb2fd584b84e0856d72
james lafferty <lafferty.james@icloud.com>
2023-01-31 23:48:05 +0000

    update to download green taxi trip data

--- week_2_workflow_orchestration/flows/etl_web_to_gcs.py
+++ week_2_workflow_orchestration/flows/etl_web_to_gcs.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import pandas as pd
+import fnmatch
 from prefect import flow, task
 from prefect_gcp.cloud_storage import GcsBucket
 
@@ -15,8 +16,12 @@ def fetch(dataset_url: str) -> pd.DataFrame:
 @task(log_prints=True)
 def clean(df=pd.DataFrame) -> pd.DataFrame:
     """Fix dtype issues"""
-    df["tpep_pickup_datetime"] = pd.to_datetime(df["tpep_pickup_datetime"])
-    df["tpep_dropoff_datetime"] = pd.to_datetime(df["tpep_dropoff_datetime"])
+    pickup_datetime = "".join(fnmatch.filter(df, "?pep_pickup_datetime"))
+    dropoff_datetime = "".join(fnmatch.filter(df, "?pep_dropoff_datetime"))
+
+    df[pickup_datetime] = pd.to_datetime(df[pickup_datetime])
+    df[dropoff_datetime] = pd.to_datetime(df[dropoff_datetime])
+
     print(df.head(2))
     print(f"columns: {df.dtypes}")
     print(f"rows: {len(df)}")
@@ -43,8 +48,8 @@ def write_gcs(path: Path) -> None:
 @flow()
 def etl_web_to_gcs() -> None:
     """The main ETL function"""
-    color = "yellow"
-    year = 2021
+    color = "green"
+    year = 2020
     month = 1
     dataset_file = f"{color}_tripdata_{year}-{month:02}"
     dataset_url = f"https://github.com/DataTalksClub/nyc-tlc-data/releases/download/{color}/{dataset_file}.csv.gz"
