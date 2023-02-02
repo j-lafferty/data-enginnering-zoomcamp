7808a086a0007ffabc1fb342d9e1e616a2154f53
james lafferty <lafferty.james@icloud.com>
2023-02-02 18:11:14 +0000

    remove infrastructure build since github cannot use it

--- week_2_workflow_orchestration/flows/github_deploy.py
+++ week_2_workflow_orchestration/flows/github_deploy.py
@@ -1,12 +1,12 @@
-from prefect.deployments import Deployment
 from prefect.filesystems import GitHub
-from parameterized_flow import etl_parent_flow
+
 
 github_block = GitHub.load("zoom-gcs")
 
-github_dep = Deployment.build_from_flow(
-    flow=etl_parent_flow, name="github-flow", infrastructure=github_block
+github_block.get_directory(  # (from_path, to_path--default=pwd)
+    "week_2_workflow_orchestration/flows", "github_sb"
 )
 
+
 if __name__ == "__main__":
-    github_dep.apply()
+    github_block.save("github_sb")
